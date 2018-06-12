09b3695c922fe53f29a99849c82e006ff39ac684
Henrique Sobral Gomes <hsobral@gmail.com>
2018-06-12 08:27:18 -0300

    old cv removal

--- opencv_engine/engine.py
+++ opencv_engine/engine.py
@@ -9,10 +9,8 @@
 # Copyright (c) 2014 globo.com timehome@corp.globo.com
 
 import uuid
-try:
-    import cv
-except ImportError:
-    import cv2.cv as cv
+
+import numpy as np
 
 from colour import Color
 from thumbor.engines import BaseEngine
@@ -20,7 +18,6 @@ from pexif import JpegFile, ExifSegment
 from types import MethodType
 import cv2
 import gdal
-import numpy
 from osgeo import osr
 
 import thumbor.utils
@@ -87,14 +84,18 @@ class Engine(BaseEngine):
     @property
     def image_depth(self):
         if self.image is None:
-            return 8
-        return cv.GetImage(self.image).depth
+            return np.uint8
+        return self.image.dtype
 
     @property
     def image_channels(self):
         if self.image is None:
             return 3
-        return self.image.channels
+        # if the image is grayscale
+        try:
+            return self.image.shape[2]
+        except IndexError:
+            return 1
 
     @classmethod
     def parse_hex_color(cls, color):
@@ -105,15 +106,16 @@ class Engine(BaseEngine):
             return None
 
     def gen_image(self, size, color_value):
-        img0 = cv.CreateImage(size, self.image_depth, self.image_channels)
         if color_value == 'transparent':
             color = (255, 255, 255, 255)
+            img = np.zeros((size[1], size[0], 4), self.image_depth)
         else:
+            img = np.zeros((size[1], size[0], self.image_channels), self.image_depth)
             color = self.parse_hex_color(color_value)
             if not color:
                 raise ValueError('Color %s is not valid.' % color_value)
-        cv.Set(img0, color)
-        return img0
+        img[:] = color
+        return img
 
     def read(self, extension=None, quality=None):
         if not extension and FORMATS[self.extension] == 'TIFF':
@@ -130,22 +132,23 @@ class Engine(BaseEngine):
             channels = None
             if hasattr(self.context, 'request') and getattr(self.context.request, 'default_to_jpeg', True):
                 if self.image_channels > 3:
-                    channels = cv2.split(numpy.asarray(self.image))
-                    if numpy.all(channels[3] == 255):
+                    channels = cv2.split(np.asarray(self.image))
+                    if np.all(channels[3] == 255):
                         self.extension = '.jpg'
 
             try:
                 if FORMATS[self.extension] == 'JPEG':
-                    options = [cv.CV_IMWRITE_JPEG_QUALITY, quality]
+                    options = [cv2.IMWRITE_JPEG_QUALITY, quality]
             except KeyError:
                 # default is JPEG so
-                options = [cv.CV_IMWRITE_JPEG_QUALITY, quality]
+                options = [cv2.IMWRITE_JPEG_QUALITY, quality]
 
             if FORMATS[self.extension] == 'TIFF':
-                channels = channels or cv2.split(numpy.asarray(self.image))
+                channels = channels or cv2.split(np.asarray(self.image))
                 data = self.write_channels_to_tiff_buffer(channels)
             else:
-                data = cv.EncodeImage(self.extension, self.image, options or []).tostring()
+                success, buf = cv2.imencode(self.extension, self.image, options or [])
+                data = buf.tostring()
 
             if FORMATS[self.extension] == 'JPEG' and self.context.config.PRESERVE_EXIF_INFO:
                 if hasattr(self, 'exif'):
@@ -171,9 +174,10 @@ class Engine(BaseEngine):
             self.buffer = buffer
             img0 = self.read_tiff(buffer, create_alpha)
         else:
-            imagefiledata = cv.CreateMatHeader(1, len(buffer), cv.CV_8UC1)
-            cv.SetData(imagefiledata, buffer, len(buffer))
-            img0 = cv.DecodeImageM(imagefiledata, cv.CV_LOAD_IMAGE_UNCHANGED)
+            img0 = cv2.imdecode(np.frombuffer(buffer, np.uint8), -1)
+            #imagefiledata = cv.CreateMatHeader(1, len(buffer), cv.CV_8UC1)
+            #cv.SetData(imagefiledata, buffer, len(buffer))
+            #img0 = cv.DecodeImageM(imagefiledata, cv.CV_LOAD_IMAGE_UNCHANGED)
 
         if FORMATS[self.extension] == 'JPEG':
             try:
@@ -203,8 +207,8 @@ class Engine(BaseEngine):
 
             if len(channels) < 4 and create_alpha:
                 self.no_data_value = gdal_img.GetRasterBand(1).GetNoDataValue()
-                channels.append(numpy.float32(gdal_img.GetRasterBand(1).GetMaskBand().ReadAsArray()))
-            return cv.fromarray(cv2.merge(channels))
+                channels.append(np.float32(gdal_img.GetRasterBand(1).GetMaskBand().ReadAsArray()))
+            return cv2.merge(channels)
         finally:
             gdal_img = None
             gdal.Unlink(mem_map_name)  # Cleanup.
@@ -274,62 +278,99 @@ class Engine(BaseEngine):
 
     @property
     def size(self):
-        return cv.GetSize(self.image)
+        return self.image.shape[1], self.image.shape[0]
 
     def normalize(self):
         pass
 
     def resize(self, width, height):
-        thumbnail = cv.CreateImage(
-            (int(round(width, 0)), int(round(height, 0))),
-            self.image_depth,
-            self.image_channels
-        )
-        cv.Resize(self.image, thumbnail, cv.CV_INTER_AREA)
-        self.image = thumbnail
+        r = height / self.size[1]
+        width = int(self.size[0] * r)
+        dim = (int(round(width, 0)), int(round(height, 0)))
+        self.image = cv2.resize(self.image, dim, interpolation=cv2.INTER_AREA)
+        #thumbnail = cv.CreateImage(
+        #    (int(round(width, 0)), int(round(height, 0))),
+        #    self.image_depth,
+        #    self.image_channels
+        #)
+        #cv.Resize(self.image, thumbnail, cv.CV_INTER_AREA)
+        #self.image = thumbnail
 
     def crop(self, left, top, right, bottom):
-        new_width = right - left
-        new_height = bottom - top
-        cropped = cv.CreateImage(
-            (new_width, new_height), self.image_depth, self.image_channels
-        )
-        src_region = cv.GetSubRect(self.image, (left, top, new_width, new_height))
-        cv.Copy(src_region, cropped)
+        #new_width = right - left
+        #new_height = bottom - top
+        #cropped = cv.CreateImage(
+        #    (new_width, new_height), self.image_depth, self.image_channels
+        #)
+        #src_region = cv.GetSubRect(self.image, (left, top, new_width, new_height))
+        #cv.Copy(src_region, cropped)
 
-        self.image = cropped
+        #self.image = cropped
+        self.image = self.image[top: bottom, left: right]
 
     def rotate(self, degrees):
-        if (degrees > 180):
-            # Flip around both axes
-            cv.Flip(self.image, None, -1)
-            degrees = degrees - 180
-
-        img = self.image
-        size = cv.GetSize(img)
-
-        if (degrees / 90 % 2):
-            new_size = (size[1], size[0])
-            center = ((size[0] - 1) * 0.5, (size[0] - 1) * 0.5)
+        # see http://stackoverflow.com/a/23990392
+        if degrees == 90:
+            self.image = cv2.transpose(self.image)
+            cv2.flip(self.image, 0, self.image)
+        elif degrees == 180:
+            cv2.flip(self.image, -1, self.image)
+        elif degrees == 270:
+            self.image = cv2.transpose(self.image)
+            cv2.flip(self.image, 1, self.image)
         else:
-            new_size = size
-            center = ((size[0] - 1) * 0.5, (size[1] - 1) * 0.5)
+            # see http://stackoverflow.com/a/37347070
+            # one pixel glitch seems to happen with 90/180/270
+            # degrees pictures in this algorithm if you check
+            # the typical github.com/recurser/exif-orientation-examples
+            # but the above transpose/flip algorithm is working fine
+            # for those cases already
+            width, height = self.size
+            image_center = (width / 2, height / 2)
+            rot_mat = cv2.getRotationMatrix2D(image_center, degrees, 1.0)
 
-        mapMatrix = cv.CreateMat(2, 3, cv.CV_64F)
-        cv.GetRotationMatrix2D(center, degrees, 1.0, mapMatrix)
-        dst = cv.CreateImage(new_size, self.image_depth, self.image_channels)
-        cv.SetZero(dst)
-        cv.WarpAffine(img, dst, mapMatrix)
-        self.image = dst
+            abs_cos = abs(rot_mat[0, 0])
+            abs_sin = abs(rot_mat[0, 1])
+            bound_w = int((height * abs_sin) + (width * abs_cos))
+            bound_h = int((height * abs_cos) + (width * abs_sin))
+
+            rot_mat[0, 2] += ((bound_w / 2) - image_center[0])
+            rot_mat[1, 2] += ((bound_h / 2) - image_center[1])
+
+            self.image = cv2.warpAffine(self.image, rot_mat, (bound_w, bound_h))
+        #if (degrees > 180):
+        #    # Flip around both axes
+        #    cv.Flip(self.image, None, -1)
+        #    degrees = degrees - 180
+
+        #img = self.image
+        #size = cv.GetSize(img)
+
+        #if (degrees / 90 % 2):
+        #    new_size = (size[1], size[0])
+        #    center = ((size[0] - 1) * 0.5, (size[0] - 1) * 0.5)
+        #else:
+        #    new_size = size
+        #    center = ((size[0] - 1) * 0.5, (size[1] - 1) * 0.5)
+
+        #mapMatrix = cv.CreateMat(2, 3, cv.CV_64F)
+        #cv.GetRotationMatrix2D(center, degrees, 1.0, mapMatrix)
+        #dst = cv.CreateImage(new_size, self.image_depth, self.image_channels)
+        #cv.SetZero(dst)
+        #cv.WarpAffine(img, dst, mapMatrix)
+        #self.image = dst
 
     def flip_vertically(self):
-        cv.Flip(self.image, None, 1)
+        self.image = np.flipud(self.image)
+        #cv.Flip(self.image, None, 1)
 
     def flip_horizontally(self):
-        cv.Flip(self.image, None, 0)
+        self.image = np.fliplr(self.image)
+        #cv.Flip(self.image, None, 0)
 
     def set_image_data(self, data):
-        cv.SetData(self.image, data)
+        self.image = np.frombuffer(data, dtype=self.image.dtype).reshape(self.image.shape)
+        #cv.SetData(self.image, data)
 
     def image_data_as_rgb(self, update_image=True):
         # TODO: Handle other formats
@@ -339,20 +380,39 @@ class Engine(BaseEngine):
             mode = 'BGR'
         else:
             mode = 'BGR'
-            rgb_copy = cv.CreateImage((self.image.width, self.image.height), 8, 3)
-            cv.CvtColor(self.image, rgb_copy, cv.CV_GRAY2BGR)
+            rgb_copy = np.zeros((self.size[1], self.size[0], 3), self.image.dtype)
+            cv2.cvtColor(self.image, cv2.COLOR_GRAY2BGR, rgb_copy)
             self.image = rgb_copy
         return mode, self.image.tostring()
+        #   rgb_copy = cv.CreateImage((self.image.width, self.image.height), 8, 3)
+        #   cv.CvtColor(self.image, rgb_copy, cv.CV_GRAY2BGR)
+        #   self.image = rgb_copy
+        #return mode, self.image.tostring()
 
     def draw_rectangle(self, x, y, width, height):
-        cv.Rectangle(self.image, (int(x), int(y)), (int(x + width), int(y + height)), cv.Scalar(255, 255, 255, 1.0))
+        cv2.rectangle(self.image, (int(x), int(y)), (int(x + width), int(y + height)), (255, 255, 255))
+        #cv.Rectangle(self.image, (int(x), int(y)), (int(x + width), int(y + height)), cv.Scalar(255, 255, 255, 1.0))
 
     def convert_to_grayscale(self):
-        if self.image_channels >= 3:
-            # FIXME: OpenCV does not support grayscale with alpha channel?
-            grayscaled = cv.CreateImage((self.image.width, self.image.height), self.image_depth, 1)
-            cv.CvtColor(self.image, grayscaled, cv.CV_BGRA2GRAY)
-            self.image = grayscaled
+        image = None
+        if self.image_channels >= 3 and with_alpha:
+            image = cv2.cvtColor(self.image, cv2.COLOR_BGRA2GRAY)
+        elif self.image_channels >= 3:
+            image = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+        elif self.image_channels == 1:
+            # Already grayscale,
+            image = self.image
+        if update_image:
+            self.image = image
+        elif self.image_depth == np.uint16:
+            #Feature detector reqiures uint8 images
+            image = np.array(image, dtype='uint8')
+        return image
+        #if self.image_channels >= 3:
+        #    # FIXME: OpenCV does not support grayscale with alpha channel?
+        #    grayscaled = cv.CreateImage((self.image.width, self.image.height), self.image_depth, 1)
+        #    cv.CvtColor(self.image, grayscaled, cv.CV_BGRA2GRAY)
+        #    self.image = grayscaled
 
     def paste(self, other_engine, pos, merge=True):
         if merge and not FILTERS_AVAILABLE:
@@ -377,11 +437,18 @@ class Engine(BaseEngine):
 
     def enable_alpha(self):
         if self.image_channels < 4:
-            with_alpha = cv.CreateImage(
-                (self.image.width, self.image.height), self.image_depth, 4
-            )
+            with_alpha = np.zeros((self.size[1], self.size[0], 4), self.image.dtype)
             if self.image_channels == 3:
-                cv.CvtColor(self.image, with_alpha, cv.CV_BGR2BGRA)
+                cv2.cvtColor(self.image, cv2.COLOR_BGR2BGRA, with_alpha)
             else:
-                cv.CvtColor(self.image, with_alpha, cv.CV_GRAY2BGRA)
+                cv2.cvtColor(self.image, cv2.COLOR_GRAY2BGRA, with_alpha)
             self.image = with_alpha
+        #if self.image_channels < 4:
+        #    with_alpha = cv.CreateImage(
+        #        (self.image.width, self.image.height), self.image_depth, 4
+        #    )
+        #    if self.image_channels == 3:
+        #        cv.CvtColor(self.image, with_alpha, cv.CV_BGR2BGRA)
+        #    else:
+        #        cv.CvtColor(self.image, with_alpha, cv.CV_GRAY2BGRA)
+        #    self.image = with_alpha
